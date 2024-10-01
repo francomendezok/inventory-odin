8f0ab3b2ea3e09413d81c24e6a1d728af6ec39f2
Franco <125481413+francomendezok@users.noreply.github.com.>
2024-10-01 09:47:52 -0300

    env and folders

--- app.mjs
+++ app.mjs
@@ -3,4 +3,4 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-app.listen(PORT => console.log(`listening on port ${PORT}`))
+app.listen(()=> console.log(`listening on port ${PORT}`))
